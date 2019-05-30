35ee3b86f6add64c2da0306e98fb7703f611a55d
丛子翔 <787043676@qq.com>
2019-05-30 19:35:34 +0800

    v1.0

---
+++ src/test/java/com/example/demo/ApplicationTests.java
@@ -0,0 +1,35 @@
+/*
+package com.example.demo;
+
+import com.example.demo.modules.sys.mapper.SysUserMapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+	@Autowired
+	private SysMenuMapper sysMenuMapper;
+	@Autowired
+	private SysUserMapper sysUserMapper;
+
+	@Test
+	public void test1() {
+		List<SysMenu> dd = sysMenuMapper.selectAll();
+		System.out.println(dd);
+	}
+
+	@Test
+	public void test2() {
+		List<SysUser> dd = sysUserMapper.selectAll();
+		System.out.println(dd);
+	}
+
+}
+*/
